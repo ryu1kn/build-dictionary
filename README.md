f3030f27ddd19b340664cbbf9569afc8c50afaeb
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-03 00:07:04 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Build Your Dictionary
+
+```sh
+$ node difficulty.js article.txt --dictionary dictionary.txt
+Words you know: 146/150 (98.0%)
+Difficulty: Easy
+```
+
+Default threshold. Should be customisable.
+
+```
+ 0 -  90%: Too difficult
+90 -  95%: Difficult
+95 -  98%: Medium
+98 - 100%: Easy
+```
---
+++ test/difficulty-calculator-spec.js
@@ -0,0 +1,23 @@
+
+const expect = require('chai').expect;
+
+const DifficultyCalculator = require('../lib/difficulty-calculator');
+
+describe('DifficultyCalculator', () => {
+
+    it('calculates the difficulty of an article against your dictionary', () => {
+        const article = 'this is an article';
+        const dictionary = article.split(' ');
+        const difficultyCalculator = new DifficultyCalculator({dictionary});
+        const result = difficultyCalculator.calculate(article);
+        expect(result).to.be.eql({difficulty: 'easy'});
+    });
+
+    it('calculates the difficulty of an article against your dictionary', () => {
+        const article = 'this is an article';
+        const dictionary = 'this is an'.split(' ');
+        const difficultyCalculator = new DifficultyCalculator({dictionary});
+        const result = difficultyCalculator.calculate(article);
+        expect(result).to.be.eql({difficulty: 'difficult'});
+    });
+});
