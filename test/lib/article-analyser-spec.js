8f88eb72f2f70dfc03125a19a25fc9fc32f360b3
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-04 00:52:42 +1000

    If there are same new words, count them as 1

--- test/lib/article-analyser-spec.js
+++ test/lib/article-analyser-spec.js
@@ -3,7 +3,7 @@ const ArticleAnalyser = require('../../lib/article-analyser');
 
 describe('ArticleAnalyser', () => {
 
-    it('analyses the difficulty of an article against your dictionary', () => {
+    it('counts new words in a given text', () => {
         const article = 'this is an article';
         const dictionary = 'this is an'.split(' ');
         const wordExtractor = {extract: text => text.split(' ')};
@@ -12,6 +12,18 @@ describe('ArticleAnalyser', () => {
         expect(result).to.be.eql({
             newWordCount: 1,
             totalWordCount: 4
+        });
+    });
+
+    it('counts the multiple same new words as 1', () => {
+        const article = 'new new word';
+        const dictionary = ['old', 'word'];
+        const wordExtractor = {extract: text => text.split(' ')};
+        const articleAnalyser = new ArticleAnalyser({dictionary, wordExtractor});
+        const result = articleAnalyser.analyse(article);
+        expect(result).to.be.eql({
+            newWordCount: 1,
+            totalWordCount: 3
         });
     });
 });
