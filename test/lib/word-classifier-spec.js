debebfb8aced418f397cdeabef711d622c1f3f40
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-04 14:38:11 +1000

    Refactored WordExtractor

---
+++ test/lib/word-classifier-spec.js
@@ -0,0 +1,27 @@
+
+const WordClassifier = require('../../lib/word-classifier');
+
+describe('WordClassifier', () => {
+
+    it('sees numbers are not a word', () => {
+        const wordClassifier = new WordClassifier();
+        expect(wordClassifier.isWord('1')).to.be.false;
+    });
+
+    it('sees isolated "-" is not a word', () => {
+        const wordClassifier = new WordClassifier();
+        expect(wordClassifier.isWord('-')).to.be.false;
+    });
+
+    it('sees time is not a word', () => {
+        const wordClassifier = new WordClassifier();
+        expect(wordClassifier.isWord('6:03PM')).to.be.false;
+    });
+
+    it('sees a roman numeral is not a word', () => {
+        const wordClassifier = new WordClassifier();
+        const tokens = ['i', 'ii', 'iv', 'v', 'x'];
+        const words = tokens.filter(wordClassifier.isWord);
+        expect(words).to.be.eql(['i']);
+    });
+});
--- test/lib/word-extractor-spec.js
+++ test/lib/word-extractor-spec.js
@@ -4,122 +4,13 @@ const WordExtractor = require('../../lib/word-extractor');
 describe('WordExtractor', () => {
 
     it('extracts a list of words from a text', () => {
-        const wordExtractor = new WordExtractor();
+        const tokeniser = {tokenise: text => text.split(' ')};
+        const wordClassifier = {isWord: token => token !== 'a'};
+        const wordExtractor = new WordExtractor({tokeniser, wordClassifier});
         const text = 'this is a text';
         expect(wordExtractor.extract(text)).to.be.eql([
-            'this', 'is', 'a', 'text'
+            'this', 'is', 'text'
         ]);
     });
 
-    it('sees "newline" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'a\nb\n\nc';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'a', 'b', 'c'
-        ]);
-    });
-
-    it('sees "," is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'this is, great';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'this', 'is', 'great'
-        ]);
-    });
-
-    it('sees "." is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'ok. great';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'ok', 'great'
-        ]);
-    });
-
-    it('sees ";" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'this; so';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'this', 'so'
-        ]);
-    });
-
-    it('sees "(" or ")" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'a) b (c';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'a', 'b', 'c'
-        ]);
-    });
-
-    it('sees "[" or "]" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'a] b [c';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'a', 'b', 'c'
-        ]);
-    });
-
-    it('sees "{" or "}" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'a} b {c';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'a', 'b', 'c'
-        ]);
-    });
-
-    it('sees "?" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'Why not?';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'Why', 'not'
-        ]);
-    });
-
-    it('sees "#" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'test #comment';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'test', 'comment'
-        ]);
-    });
-
-    it('sees \'"\" is not a part of a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'thought "why not"';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'thought', 'why', 'not'
-        ]);
-    });
-
-    it('sees numbers are not a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = '1 word 23';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'word'
-        ]);
-    });
-
-    it('sees isolated "-" is not a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'melbourne - sydney';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'melbourne', 'sydney'
-        ]);
-    });
-
-    it('sees time is not a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'at 6:03PM tomorrow';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'at', 'tomorrow'
-        ]);
-    });
-
-    it('sees a roman numeral is not a word', () => {
-        const wordExtractor = new WordExtractor();
-        const text = 'i am ii) a iii) b iv) c xi) d';
-        expect(wordExtractor.extract(text)).to.be.eql([
-            'i', 'am', 'a', 'b', 'c', 'd'
-        ]);
-    });
 });
