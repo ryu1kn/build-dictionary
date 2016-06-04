b13d5288e5635ddf00f23caedf091042ca012a43
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-04 20:25:29 +1000

    Doesn't print new word section if there are no new words

--- test/lib/analysis-formatter-spec.js
+++ test/lib/analysis-formatter-spec.js
@@ -3,7 +3,7 @@ const AnalysisFormatter = require('../../lib/analysis-formatter');
 
 describe('AnalysisFormatter', () => {
 
-    it('formats analysis', () => {
+    it('returns a string describing the result of analysis', () => {
         const analysisFormatter = new AnalysisFormatter();
         const analysis = {
             newWordCount: 3,
@@ -18,6 +18,19 @@ describe('AnalysisFormatter', () => {
             'NEW_WORD1',
             'NEW_WORD2',
             'NEW_WORD3'
+        ].join('\n'));
+    });
+
+    it("doesn't provide new words list if there are no new words", () => {
+        const analysisFormatter = new AnalysisFormatter();
+        const analysis = {
+            newWordCount: 0,
+            newWords: [],
+            totalWordCount: 150
+        };
+        expect(analysisFormatter.format(analysis, 'DIFFICULTY')).to.be.equal([
+            'New words: 0/150 (0.0%)',
+            'Difficulty: DIFFICULTY'
         ].join('\n'));
     });
 
