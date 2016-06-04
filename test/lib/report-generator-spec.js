ee9e8e23904e199507ae298c5a8e066bd9a158f1
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-05 00:12:14 +1000

    Rename AnalysisFormatter to ReportGenerator

--- test/lib/report-generator-spec.js
+++ test/lib/report-generator-spec.js
@@ -1,16 +1,16 @@
 
-const AnalysisFormatter = require('../../lib/analysis-formatter');
+const ReportGenerator = require('../../lib/report-generator');
 
-describe('AnalysisFormatter', () => {
+describe('ReportGenerator', () => {
 
     it('returns a string describing the result of analysis', () => {
-        const analysisFormatter = new AnalysisFormatter();
+        const reportGenerator = new ReportGenerator();
         const analysis = {
             newWordCount: 3,
             newWords: ['NEW_WORD1', 'NEW_WORD2', 'NEW_WORD3'],
             totalWordCount: 150
         };
-        expect(analysisFormatter.format(analysis, 'DIFFICULTY')).to.be.equal([
+        expect(reportGenerator.generate(analysis, 'DIFFICULTY')).to.be.equal([
             'New words: 3/150 (2.0%)',
             'Difficulty: DIFFICULTY',
             '',
@@ -22,13 +22,13 @@ describe('AnalysisFormatter', () => {
     });
 
     it("doesn't provide new words list if there are no new words", () => {
-        const analysisFormatter = new AnalysisFormatter();
+        const reportGenerator = new ReportGenerator();
         const analysis = {
             newWordCount: 0,
             newWords: [],
             totalWordCount: 150
         };
-        expect(analysisFormatter.format(analysis, 'DIFFICULTY')).to.be.equal([
+        expect(reportGenerator.generate(analysis, 'DIFFICULTY')).to.be.equal([
             'New words: 0/150 (0.0%)',
             'Difficulty: DIFFICULTY'
         ].join('\n'));
