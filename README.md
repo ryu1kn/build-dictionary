05fa42ddc175230498115a0e3c787a9bba2b8fd5
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-04 15:02:28 +1000

    Made the difficulty rating match to README

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Difficulty: Easy
 Default threshold. Should be customisable.
 
 ```
-10 -    : Too difficult
+10 -    : Very difficult
  5 - 10%: Difficult
  2 -  5%: Medium
  0 -  2%: Easy
--- test/lib/difficulty-rater-spec.js
+++ test/lib/difficulty-rater-spec.js
@@ -3,12 +3,27 @@ const DifficultyRater = require('../../lib/difficulty-rater');
 
 describe('DifficultyRater', () => {
 
-    it('retes the difficulty based on the given article analysis', () => {
+    it("says it's very difficult to read if new words are more than 10%", () => {
         const difficultyRater = new DifficultyRater();
-        const analysis = {
-            newWordCount: 1,
-            totalWordCount: 4
-        };
+        const analysis = {newWordCount: 11, totalWordCount: 100};
+        expect(difficultyRater.rate(analysis)).to.be.eql('Very difficult');
+    });
+
+    it("says it's difficult to read if new words are at most 10%", () => {
+        const difficultyRater = new DifficultyRater();
+        const analysis = {newWordCount: 10, totalWordCount: 100};
         expect(difficultyRater.rate(analysis)).to.be.eql('Difficult');
+    });
+
+    it("says it's medium to read if new words are at most 2%", () => {
+        const difficultyRater = new DifficultyRater();
+        const analysis = {newWordCount: 5, totalWordCount: 100};
+        expect(difficultyRater.rate(analysis)).to.be.eql('Medium');
+    });
+
+    it("says it's easy to read if new words are at most 2%", () => {
+        const difficultyRater = new DifficultyRater();
+        const analysis = {newWordCount: 2, totalWordCount: 100};
+        expect(difficultyRater.rate(analysis)).to.be.eql('Easy');
     });
 });
