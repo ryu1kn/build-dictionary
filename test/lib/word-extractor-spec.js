e76d5aaa4b1098602eb34fd3c2eba9644ccbc766
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-06 00:56:39 +1000

    Treat a proper noun as non general words

--- test/lib/word-extractor-spec.js
+++ test/lib/word-extractor-spec.js
@@ -23,4 +23,19 @@ describe('WordExtractor', () => {
         ]);
     });
 
+    it("doesn't recognise a proper noun as a general word", () => {
+        const tokeniser = {tokenise: text => text.split(' ')};
+        const wordClassifier = {isWord: () => true};
+        const wordExtractor = new WordExtractor({tokeniser, wordClassifier});
+        const text = 'This is Malcolm. That is Bill.';
+        expect(wordExtractor.extract(text)).to.be.eql(['This', 'is', 'That']);
+    });
+
+    it("sees the first word in a sentence as a general word even if it's a proper noun", () => {
+        const tokeniser = {tokenise: text => text.split(' ')};
+        const wordClassifier = {isWord: () => true};
+        const wordExtractor = new WordExtractor({tokeniser, wordClassifier});
+        const text = 'This is Malcolm. Bill is there';
+        expect(wordExtractor.extract(text)).to.be.eql(['This', 'is', 'Bill', 'there']);
+    });
 });
