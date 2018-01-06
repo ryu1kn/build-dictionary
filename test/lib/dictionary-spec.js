1ed7f1f8e0fa409757fa5c8f8c75b4e10632ccec
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-01-06 23:57:02 +1100

    Dictionary now receive FileReader instead of fs

--- test/lib/dictionary-spec.js
+++ test/lib/dictionary-spec.js
@@ -2,16 +2,16 @@ const Dictionary = require('../../lib/dictionary')
 
 describe('Dictionary', () => {
   it('returns true if a word is known', async () => {
-    const fs = { readFile: sinon.stub().callsArgWith(2, null, 'word1\nword2') }
+    const fileReader = { read: () => Promise.resolve('word1\nword2') }
     const filePath = 'FILE_PATH'
-    const dictionary = new Dictionary({ fs, filePath })
+    const dictionary = new Dictionary({ fileReader, filePath })
     expect(await dictionary.exists('word1')).to.be.true
   })
 
   it('returns false if a word is unknown', async () => {
-    const fs = { readFile: sinon.stub().callsArgWith(2, null, 'word1\nword2') }
+    const fileReader = { read: () => Promise.resolve('word1\nword2') }
     const filePath = 'FILE_PATH'
-    const dictionary = new Dictionary({ fs, filePath })
+    const dictionary = new Dictionary({ fileReader, filePath })
     expect(await dictionary.exists('unknown')).to.be.false
   })
 })
