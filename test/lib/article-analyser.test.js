81f9cbfd8576cba8db2a01dc24ebbac9cf3d950c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-01-11 21:15:51 +1100

    Report the same new words once

--- test/lib/article-analyser.test.js
+++ test/lib/article-analyser.test.js
@@ -4,11 +4,10 @@ const ArticleAnalyser = require('../../lib/article-analyser')
 
 describe('ArticleAnalyser', () => {
   it('counts new words in a given text', async () => {
-    const article = 'this is an article'
-    const dictionary = stubDictionary(['this', 'is', 'an'])
-    const wordExtractor = { extract: text => text.split(' ') }
-    const articleAnalyser = new ArticleAnalyser({ dictionary, wordExtractor })
-    const result = await articleAnalyser.analyse(article)
+    const articleAnalyser = createArticleAnalyser({
+      knownWords: ['this', 'is', 'an']
+    })
+    const result = await articleAnalyser.analyse('this is an article')
     expect(result).to.be.eql({
       newWordCount: 1,
       newWords: ['article'],
@@ -16,7 +15,21 @@ describe('ArticleAnalyser', () => {
     })
   })
 
-  function stubDictionary (knownWords) {
-    return { exists: word => Promise.resolve(knownWords.includes(word)) }
+  it('counts as one if multiple same words are recognised as new words', async () => {
+    const articleAnalyser = createArticleAnalyser({ knownWords: ['words'] })
+    const result = await articleAnalyser.analyse('new new words')
+    expect(result).to.be.eql({
+      newWordCount: 1,
+      newWords: ['new'],
+      totalWordCount: 3
+    })
+  })
+
+  function createArticleAnalyser ({ article, knownWords }) {
+    const dictionary = {
+      exists: word => Promise.resolve(knownWords.includes(word))
+    }
+    const wordExtractor = { extract: text => text.split(' ') }
+    return new ArticleAnalyser({ dictionary, wordExtractor })
   }
 })
