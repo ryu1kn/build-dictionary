28077df67d0f29f9010190ec8beb3e15a6b59926
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-05-06 20:25:09 +1000

    Replaced `tail -r` with POSIX standard `tac`

--- destroy-system.sh
+++ destroy-system.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-MODULES=(`cat ./module-deploy-order.txt | grep -v '^#' | tail -r`)
+MODULES=(`cat ./module-deploy-order.txt | grep -v '^#' | tac`)
 
 for MODULE in ${MODULES[*]} ; do
     (cd modules/$MODULE && yarn run destroy --env $ENV_NAME --region ap-southeast-2)
