712df44cbb61ef2044729fe6cf449b936b230678
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-12-30 17:51:50 +1100

    Separate analyse-article-command test as an integration test

---
+++ test-integration/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+this_dir="$(dirname "$0")"
+emphasize='\e[1;31m'
+reset='\e[0m'
+
+expected=$(cat << EOF
+New words: 1/3 (33.3%)
+Difficulty: Very difficult
+
+= New words =
+unknown
+EOF
+)
+
+actual=$("$this_dir/../bin/analyse-article" --dictionary fixtures/dictionary.txt fixtures/article.txt)
+
+if [[ "$actual" = "$expected" ]] ; then
+    echo '...Passed 👍'
+else
+    printf "${emphasize}Expected${reset} \"%s\"\n\n${emphasize}But got${reset} \"%s\"\n" "$expected" "$actual"
+    exit 1
+fi
