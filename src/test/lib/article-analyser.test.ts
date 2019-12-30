e5d8899aab4a5dbf90d74ae182a9434d3a813675
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-12-30 23:44:55 +1100

    Reduced mocks and found an error in test...

--- src/test/lib/article-analyser.test.ts
+++ src/test/lib/article-analyser.test.ts
@@ -1,11 +1,15 @@
 import { deepStrictEqual } from 'assert'
-import {ArticleAnalyser} from '../../lib/article-analyser'
+import { ArticleAnalyser } from '../../lib/article-analyser'
 import { WordExtractor } from '../../lib/word-extractor'
 import { Dictionary } from '../../lib/dictionary'
+import { Tokeniser } from '../../lib/tokeniser'
+import { WordClassifier } from '../../lib/word-classifier'
 
 describe('ArticleAnalyser', () => {
+  const toWords = (s: string) => s.split('/').join('\n')
+
   it('counts new words in a given text', async () => {
-    const articleAnalyser = createArticleAnalyser(['this', 'is', 'an'])
+    const articleAnalyser = createArticleAnalyser('this/is/an')
     const result = await articleAnalyser.analyse('this is an article')
     deepStrictEqual(result, {
       newWordCount: 1,
@@ -15,20 +19,19 @@ describe('ArticleAnalyser', () => {
   })
 
   it('counts as one if multiple same words are recognised as new words', async () => {
-    const articleAnalyser = createArticleAnalyser(['words'])
+    const articleAnalyser = createArticleAnalyser('words')
     const result = await articleAnalyser.analyse('new new words')
     deepStrictEqual(result, {
       newWordCount: 1,
       newWords: ['new'],
-      totalWordCount: 3
+      totalWordCount: 2
     })
   })
 
-  function createArticleAnalyser (knownWords: string[]) {
-    const dictionary = {
-      exists: (word: string) => Promise.resolve(knownWords.includes(word))
-    }
-    const wordExtractor = { extract: (text: string) => text.split(' ') }
-    return new ArticleAnalyser(wordExtractor as WordExtractor, dictionary as Dictionary)
+  function createArticleAnalyser (words: string) {
+    const readFile = () => Promise.resolve(toWords(words))
+    const dictionary = new Dictionary('foo' , readFile)
+    const wordExtractor = new WordExtractor(new Tokeniser(), new WordClassifier())
+    return new ArticleAnalyser(wordExtractor, dictionary)
   }
 })
