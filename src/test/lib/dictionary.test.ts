54929d4f25f000d00819567690a2cc1e5f3be666
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-12-31 12:37:53 +1100

    More DRY in test

--- src/test/lib/dictionary.test.ts
+++ src/test/lib/dictionary.test.ts
@@ -3,17 +3,15 @@ import { equal } from 'assert'
 import {Dictionary} from '../../lib/dictionary'
 
 describe('Dictionary', () => {
+  const readFile = () => Promise.resolve('word1\nword2')
+  const filePath = 'FILE_PATH'
+  const dictionary = new Dictionary(filePath, readFile)
+
   it('returns true if a word is known', async () => {
-    const readFile = () => Promise.resolve('word1\nword2')
-    const filePath = 'FILE_PATH'
-    const dictionary = new Dictionary(filePath, readFile)
     equal(await dictionary.exists('word1'), true)
   })
 
   it('returns false if a word is unknown', async () => {
-    const readFile = () => Promise.resolve('word1\nword2')
-    const filePath = 'FILE_PATH'
-    const dictionary = new Dictionary(filePath, readFile)
     equal(await dictionary.exists('unknown'), false)
   })
 })
--- src/test/lib/word-classifier.test.ts
+++ src/test/lib/word-classifier.test.ts
@@ -3,23 +3,21 @@ import { deepStrictEqual, equal } from 'assert'
 import {WordClassifier} from '../../lib/word-classifier'
 
 describe('WordClassifier', () => {
+  const wordClassifier = new WordClassifier()
+
   it('sees numbers are not a word', () => {
-    const wordClassifier = new WordClassifier()
     equal(wordClassifier.isWord('1'), false)
   })
 
   it('sees isolated "-" is not a word', () => {
-    const wordClassifier = new WordClassifier()
     equal(wordClassifier.isWord('-'), false)
   })
 
   it('sees time is not a word', () => {
-    const wordClassifier = new WordClassifier()
     equal(wordClassifier.isWord('6:03PM'), false)
   })
 
   it('sees a roman numeral is not a word', () => {
-    const wordClassifier = new WordClassifier()
     const tokens = ['i', 'ii', 'iv', 'v', 'x']
     const words = tokens.filter(wordClassifier.isWord)
     deepStrictEqual(words, ['i'])
