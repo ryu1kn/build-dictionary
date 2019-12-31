14d05e2d17185f0ead6cd016861b1b24f69dcbe8
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-12-31 15:23:46 +1100

    Removed duplicate code in test

--- src/test/lib/find-all-words.test.ts
+++ src/test/lib/find-all-words.test.ts
@@ -4,70 +4,59 @@ import {WordExtractor} from '../../lib/word-extractor'
 
 describe('WordExtractor', () => {
   const wordExtractor = new WordExtractor()
+  const assertWords = (text: string, words: string[]) =>
+    deepStrictEqual(wordExtractor.extract(text), words)
 
   it('extracts a list of words from a text', () => {
-    const text = 'this is a text'
-    deepStrictEqual(wordExtractor.extract(text), ['this', 'is', 'a', 'text'])
+    assertWords('this is a text', ['this', 'is', 'a', 'text'])
   })
 
   it('sees "newline" is not a part of a word', () => {
-    const text = 'a\nb\n\nc'
-    deepStrictEqual(wordExtractor.extract(text), ['a', 'b', 'c'])
+    assertWords('a\nb\n\nc', ['a', 'b', 'c'])
   })
 
   it('sees "," is not a part of a word', () => {
-    const text = 'this is, great'
-    deepStrictEqual(wordExtractor.extract(text), ['this', 'is', 'great'])
+    assertWords('this is, great', ['this', 'is', 'great'])
   })
 
   it('sees "." is not a part of a word', () => {
-    const text = 'ok. great'
-    deepStrictEqual(wordExtractor.extract(text), ['ok', 'great'])
+    assertWords('ok. great', ['ok', 'great'])
   })
 
   it('sees ";" is not a part of a word', () => {
-    const text = 'this; so'
-    deepStrictEqual(wordExtractor.extract(text), ['this', 'so'])
+    assertWords('this; so', ['this', 'so'])
   })
 
   it('sees "(" or ")" is not a part of a word', () => {
-    const text = 'a) b (c'
-    deepStrictEqual(wordExtractor.extract(text), ['a', 'b', 'c'])
+    assertWords('a) b (c', ['a', 'b', 'c'])
   })
 
   it('sees "[" or "]" is not a part of a word', () => {
-    const text = 'a] b [c'
-    deepStrictEqual(wordExtractor.extract(text), ['a', 'b', 'c'])
+    assertWords('a] b [c', ['a', 'b', 'c'])
   })
 
   it('sees "{" or "}" is not a part of a word', () => {
-    const text = 'a} b {c'
-    deepStrictEqual(wordExtractor.extract(text), ['a', 'b', 'c'])
+    assertWords('a} b {c', ['a', 'b', 'c'])
   })
 
   it('sees "?" is not a part of a word', () => {
-    const text = 'Why not?'
-    deepStrictEqual(wordExtractor.extract(text), ['Why', 'not'])
+    assertWords('Why not?', ['Why', 'not'])
   })
 
   it('sees "#" is not a part of a word', () => {
-    const text = 'test #comment'
-    deepStrictEqual(wordExtractor.extract(text), ['test', 'comment'])
+    assertWords('test #comment', ['test', 'comment'])
   })
 
   it("sees '\"' is not a part of a word", () => {
-    const text = 'thought "why not"'
-    deepStrictEqual(wordExtractor.extract(text), ['thought', 'why', 'not'])
+    assertWords('thought "why not"', ['thought', 'why', 'not'])
   })
 
   it('sees "\'" is not a part of a word', () => {
-    const text = "'Rot' is"
-    deepStrictEqual(wordExtractor.extract(text), ['Rot', 'is'])
+    assertWords("'Rot' is", ['Rot', 'is'])
   })
 
   it('counts the multiple same words as 1', () => {
-    const text = 'this is a long long long text'
-    deepStrictEqual(wordExtractor.extract(text), [
+    assertWords('this is a long long long text', [
       'this',
       'is',
       'a',
@@ -77,13 +66,11 @@ describe('WordExtractor', () => {
   })
 
   it("doesn't recognise a proper noun as a general word", () => {
-    const text = 'This is Malcolm. That is Bill.'
-    deepStrictEqual(wordExtractor.extract(text), ['This', 'is', 'That'])
+    assertWords('This is Malcolm. That is Bill.', ['This', 'is', 'That'])
   })
 
   it("sees the first word in a sentence as a general word even if it's a proper noun", () => {
-    const text = 'This is Malcolm. Bill is there'
-    deepStrictEqual(wordExtractor.extract(text), [
+    assertWords('This is Malcolm. Bill is there', [
       'This',
       'is',
       'Bill',
@@ -92,15 +79,15 @@ describe('WordExtractor', () => {
   })
 
   it('sees numbers are not a word', () => {
-    deepStrictEqual(wordExtractor.extract('number 1'), ['number'])
+    assertWords('number 1', ['number'])
   })
 
   it('sees isolated "-" is not a word', () => {
-    deepStrictEqual(wordExtractor.extract('here - there'), ['here', 'there'])
+    assertWords('here - there', ['here', 'there'])
   })
 
   it('sees time is not a word', () => {
-    deepStrictEqual(wordExtractor.extract('Meet at 6:03PM'), ['Meet', 'at'])
+    assertWords('Meet at 6:03PM', ['Meet', 'at'])
   })
 
   it('sees a roman numeral is not a word', () => {
@@ -111,6 +98,6 @@ describe('WordExtractor', () => {
       v. five
       x. ten
     `
-    deepStrictEqual(wordExtractor.extract(article), ['i', 'one', 'two', 'four', 'five', 'ten'])
+    assertWords(article, ['i', 'one', 'two', 'four', 'five', 'ten'])
   })
 })
